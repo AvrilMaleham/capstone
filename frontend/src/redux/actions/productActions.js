6c12fe2d65d01de0c021803ee0cd4b32366752aa
Avril Maleham <avrilmaleham@outlook.com>
2023-03-31 17:41:33 +1300

    31March23-1

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -26,7 +26,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({type: actionTypes.GET_PRODUCT_DETAILS_REQUEST});
 
-        const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/products/${id}`);
+        const { data } = await axios.get(`${window.location.origin}:3001/api/products/${id}`);
 
         dispatch ({
             type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
