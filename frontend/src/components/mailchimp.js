c0f83850c75799370655994b41937b072bcee51c
Avril Maleham <avrilmaleham@outlook.com>
2023-07-24 18:53:56 +1200

    adding erins mailchimp to contact form

--- frontend/src/components/mailchimp.js
+++ frontend/src/components/mailchimp.js
@@ -23,7 +23,7 @@ export const MailchimpNewsletter = (props) => {
 };
 
 export const MailchimpContactForm = (props) => {
-  const postUrl = `https://outlook.us21.list-manage.com/subscribe/post?u=0be82fdd5ab574d0cfd70848a&id=205041509b`;
+  const postUrl = `https://edifypmconsulting.us5.list-manage.com/subscribe/post?u=2398b719196b0b2ccde37d6fb&id=1b4cf6ceb2`;
 
   return (
     <div className="mc__form-container">
