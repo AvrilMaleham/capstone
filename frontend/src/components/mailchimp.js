39c61b13a8a0beae7de61c9a2431f1ee27c50084
Avril Maleham <avrilmaleham@outlook.com>
2023-07-24 18:40:36 +1200

    erins details added for mailchimp

--- frontend/src/components/mailchimp.js
+++ frontend/src/components/mailchimp.js
@@ -4,7 +4,7 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import ContactForm from "./contactForm";
 
 export const MailchimpNewsletter = (props) => {
-  const postUrl = `https://outlook.us21.list-manage.com/subscribe/post?u=0be82fdd5ab574d0cfd70848a&id=205041509b`;
+  const postUrl = `https://edifypmconsulting.us5.list-manage.com/subscribe/post?u=2398b719196b0b2ccde37d6fb&id=1b4cf6ceb2`;
 
   return (
     <div className="mc__form-container">
